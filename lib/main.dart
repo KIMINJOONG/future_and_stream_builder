5eb4c9c71fa0c7f8d10fdaa7b4b722201705482e
KIMINJOONG <ijboym216@gmail.com>
2022-09-08 12:19:45 +0900

    1. future builder 뽀개기

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_and_stream_builder/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
